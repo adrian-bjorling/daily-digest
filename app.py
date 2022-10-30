039a5763a5ec098d2637a213c862169a7b687b07
adrian-bjorling <98982479+adrian-bjorling@users.noreply.github.com>
2022-10-30 14:58:38 +0100

    fixed typo

--- app.py
+++ app.py
@@ -53,10 +53,10 @@ def construct_message(planner):
     for date in jobs.items():
         local_msg = f"{date[0]} - {date[1]['job']}.\n"
         if date[1]["job"] != "Oplanerad" and date[1]["team"] != []:
-            local_msg = local_msg + f"\nMed:\n"
+            local_msg = local_msg + f"Med:\n"
             for name in date[1]["team"]:
                 local_msg = local_msg + f"{name}\n"
-            local_msg = local_message + "\n"
+            local_msg = local_msg + "\n"
         msg.append(local_msg)
 
     return msg
